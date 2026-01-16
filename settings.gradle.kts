604cbd174fe5edc2d318fe583ebf5a799a60d08d
vinendrakartik <41591008+vinendrakartik@users.noreply.github.com>
2026-01-17 00:59:32 +0530

    Update root project name and remove features

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,5 @@ dependencyResolutionManagement {
         maven { setUrl("https://jitpack.io") }
     }
 }
-rootProject.name = "Fossify-Commons"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-include(":commons", ":samples")
+rootProject.name = "Commons"
+
