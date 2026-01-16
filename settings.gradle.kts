6b62343a9cd0a8fd2722e1aae8ae746e38c644ab
vinendrakartik <41591008+vinendrakartik@users.noreply.github.com>
2026-01-17 01:23:21 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,16 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
+        // CRITICAL: PatternLockView and RTL-ViewPager live here
+        maven { url = uri("https://jcenter.bintray.com") } 
     }
 }
+
 rootProject.name = "Commons"
