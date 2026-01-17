d36c97b2eedd145c16eb92165a0eebcf211fe12a
vinendrakartik <41591008+vinendrakartik@users.noreply.github.com>
2026-01-17 09:54:03 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,8 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 dependencyResolutionManagement {
-    // Keep this strict mode! It's good practice.
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-        // We move the required repo HERE
-        maven { url = uri("https://jcenter.bintray.com") } 
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+  }
 }
-
-rootProject.name = "Commons"
